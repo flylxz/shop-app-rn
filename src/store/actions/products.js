5093fc2928f1d7b4e15dde7337b5e5fab40dbfca
Александр Гладченко <front.end.aam@users-MacBook-Pro.local>
2021-09-29 10:36:45 +0300

    updating product

--- src/store/actions/products.js
+++ src/store/actions/products.js
@@ -1,6 +1,28 @@
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
+export const CREATE_PRODUCT = 'CREATE_PRODUCT';
+export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 
 export const deleteProduct = (productId) => ({
   type: DELETE_PRODUCT,
   pid: productId,
+});
+
+export const createProduct = (title, imageUrl, description, price) => ({
+  type: CREATE_PRODUCT,
+  productDATA: {
+    title,
+    description,
+    imageUrl,
+    price,
+  },
+});
+
+export const updateProduct = (id, title, imageUrl, description) => ({
+  type: UPDATE_PRODUCT,
+  pid: id,
+  productDATA: {
+    title,
+    description,
+    imageUrl,
+  },
 });
--- src/store/reducers/products.js
+++ src/store/reducers/products.js
@@ -1,5 +1,10 @@
 import PRODUCTS from '../../data/dummy-data';
-import { DELETE_PRODUCT } from '../actions/products';
+import Product from '../../models/product';
+import {
+  CREATE_PRODUCT,
+  DELETE_PRODUCT,
+  UPDATE_PRODUCT,
+} from '../actions/products';
 
 const initialState = {
   availableProducts: PRODUCTS,
@@ -8,6 +13,39 @@ const initialState = {
 
 export const productReducer = (state = initialState, action) => {
   switch (action.type) {
+    case CREATE_PRODUCT:
+      const newProduct = new Product(
+        new Date().toString(),
+        'u1',
+        action.productData.title,
+        action.productData.description,
+        action.productData.imageUrl,
+        action.productData.price
+      );
+
+      return {
+        ...state,
+        availableProducts: [...state.availableProducts, newProduct],
+        userProducts: [...state.userProducts, newProduct],
+      };
+
+    case UPDATE_PRODUCT:
+      const productIdx = state.userProducts.findIndex(
+        (prod) => prod.id === action.pid
+      );
+
+      const updatedProduct = new Product(
+        action.pid,
+        state.userProducts[productIdx].ownerId,
+        action.productData.title,
+        action.productData.imageUrl,
+        action.productData.description,
+        state.userProducts[productIdx].price
+      );
+
+      const updatedUserProducts = console.log('update');
+      return { ...state };
+
     case DELETE_PRODUCT:
       return {
         ...state,
